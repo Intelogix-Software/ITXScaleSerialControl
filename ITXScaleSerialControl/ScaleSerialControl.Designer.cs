c6ede3672c6c27a169aa2b0efe55ee96fadb9f27
RaulRSB <rauls@intelogix.mx>
2022-03-11 08:35:31 -0700

    update to read.existing

--- ITXScaleSerialControl/ScaleSerialControl.Designer.cs
+++ ITXScaleSerialControl/ScaleSerialControl.Designer.cs
@@ -31,37 +31,92 @@
             this.components = new System.ComponentModel.Container();
             this.port_1 = new System.IO.Ports.SerialPort(this.components);
             this.panelControl1 = new DevExpress.XtraEditors.PanelControl();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.lbl_currentValue = new DevExpress.XtraEditors.LabelControl();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.lbl_motion = new DevExpress.XtraEditors.LabelControl();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.bw_serialRead = new System.ComponentModel.BackgroundWorker();
             ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).BeginInit();
             this.panelControl1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // port_1
+            // 
+            this.port_1.PortName = "COM2";
             // 
             // panelControl1
             // 
-            this.panelControl1.Controls.Add(this.lbl_currentValue);
+            this.panelControl1.Controls.Add(this.panel1);
+            this.panelControl1.Controls.Add(this.panel2);
+            this.panelControl1.Controls.Add(this.flowLayoutPanel1);
             this.panelControl1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panelControl1.Location = new System.Drawing.Point(0, 0);
             this.panelControl1.Name = "panelControl1";
             this.panelControl1.Size = new System.Drawing.Size(421, 98);
             this.panelControl1.TabIndex = 0;
+            this.panelControl1.Paint += new System.Windows.Forms.PaintEventHandler(this.panelControl1_Paint);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lbl_currentValue);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(2, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(265, 94);
+            this.panel1.TabIndex = 2;
             // 
             // lbl_currentValue
             // 
-            this.lbl_currentValue.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbl_currentValue.Appearance.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_currentValue.Appearance.BackColor2 = System.Drawing.Color.Transparent;
             this.lbl_currentValue.Appearance.Font = new System.Drawing.Font("Tahoma", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_currentValue.Appearance.Options.UseBackColor = true;
             this.lbl_currentValue.Appearance.Options.UseFont = true;
+            this.lbl_currentValue.Dock = System.Windows.Forms.DockStyle.Fill;
             this.lbl_currentValue.Enabled = false;
-            this.lbl_currentValue.Location = new System.Drawing.Point(5, 5);
+            this.lbl_currentValue.Location = new System.Drawing.Point(0, 0);
             this.lbl_currentValue.Name = "lbl_currentValue";
             this.lbl_currentValue.Size = new System.Drawing.Size(314, 77);
             this.lbl_currentValue.TabIndex = 0;
             this.lbl_currentValue.Text = "SerialValue";
             // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.lbl_motion);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel2.Location = new System.Drawing.Point(267, 2);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(152, 94);
+            this.panel2.TabIndex = 4;
+            // 
+            // lbl_motion
+            // 
+            this.lbl_motion.Appearance.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_motion.Appearance.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_motion.Appearance.Options.UseBackColor = true;
+            this.lbl_motion.Appearance.Options.UseFont = true;
+            this.lbl_motion.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lbl_motion.Enabled = false;
+            this.lbl_motion.ImageOptions.Image = global::ITXScaleSerialControl.Properties.Resources.merge_32x32;
+            this.lbl_motion.Location = new System.Drawing.Point(0, 0);
+            this.lbl_motion.Name = "lbl_motion";
+            this.lbl_motion.Size = new System.Drawing.Size(32, 32);
+            this.lbl_motion.TabIndex = 1;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(2, 37);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(8, 8);
+            this.flowLayoutPanel1.TabIndex = 3;
+            // 
             // bw_serialRead
             // 
             this.bw_serialRead.WorkerReportsProgress = true;
+            this.bw_serialRead.WorkerSupportsCancellation = true;
             this.bw_serialRead.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bw_serialRead_DoWork);
             this.bw_serialRead.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.bw_serialRead_ProgressChanged);
             this.bw_serialRead.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.bw_serialRead_RunWorkerCompleted);
@@ -76,16 +131,22 @@
             this.Load += new System.EventHandler(this.ScaleSerialControl_Load);
             ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).EndInit();
             this.panelControl1.ResumeLayout(false);
-            this.panelControl1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.IO.Ports.SerialPort port_1;
         private DevExpress.XtraEditors.PanelControl panelControl1;
         private System.ComponentModel.BackgroundWorker bw_serialRead;
         private DevExpress.XtraEditors.LabelControl lbl_currentValue;
+        private DevExpress.XtraEditors.LabelControl lbl_motion;
+        public System.IO.Ports.SerialPort port_1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
     }
 }
--- ITXScaleSerialControl/ScaleSerialControl.cs
+++ ITXScaleSerialControl/ScaleSerialControl.cs
@@ -8,47 +8,50 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO.Ports;
+using DevExpress.Utils;
+using System.Threading;
 
 namespace ITXScaleSerialControl
 {
     public partial class ScaleSerialControl: UserControl
     {
         public int fontSizeV=40;
-        public new event EventHandler Click
+        //public new event EventHandler Click
+        //{
+        //    add
+        //    {
+        //        base.Click += value;
+        //        foreach (Control control in Controls)
+        //        {
+        //            control.Click += value;
+        //        }
+        //    }
+        //    remove
+        //    {
+        //        base.Click -= value;
+        //        foreach (Control control in Controls)
+        //        {
+        //            control.Click -= value;
+        //        }
+        //    }
+        //}
+        public new event EventHandler DoubleClick
         {
             add
             {
                 base.Click += value;
-                foreach (Control control in Controls)
+                foreach (Control control in this.Controls)
                 {
-                    control.Click += value;
+                            control.Click += value;
                 }
+
             }
             remove
             {
                 base.Click -= value;
                 foreach (Control control in Controls)
                 {
-                    control.Click -= value;
-                }
-            }
-        }
-        public new event EventHandler dClick
-        {
-            add
-            {
-                base.DoubleClick += value;
-                foreach (Control control in Controls)
-                {
-                    control.Click += value;
-                }
-            }
-            remove
-            {
-                base.DoubleClick -= value;
-                foreach (Control control in Controls)
-                {
-                    control.Click -= value;
+                            control.Click += value;
                 }
             }
         }
@@ -76,45 +79,197 @@ namespace ITXScaleSerialControl
             get { return lbl_currentValue.ForeColor; }
             set { lbl_currentValue.ForeColor = value; }
         }
+        public Image lblMotionImage
+        {
+            get { return lbl_motion.ImageOptions.Image; }
+            set { lbl_motion.ImageOptions.Image = value; }
+        }
+        public Font lblMotion
+        {
+            get { return lbl_motion.Font; }
+            set { lbl_motion.Font = value; }
+        }
+        private string lblm;
+        public string lblMotionText
+        {
+            get { return lblm; }
+            set { lblm = value; }
+        }
+        public Color ForeMotion
+        {
+            get { return lbl_motion.ForeColor; }
+            set { lbl_motion.ForeColor = value; }
+        }
+
+
         public string CurrentValue;
-        
+        public bool motionDetected;
+        public bool isTare;
+        public bool isLB;
+        public bool cancel=false;
+
+        private Queue<byte> recievedData = new Queue<byte>();
+
         public ScaleSerialControl()
         {
             InitializeComponent();
             //lbl_currentValue.Font = new Font(lbl_currentValue.Font.FontFamily,100);
+            //StartRead();
 
         }
         public void StartRead()
         {
             bw_serialRead.RunWorkerAsync();
+            //System.Windows.Forms.MessageBox.Show(port_1.PortName);
         }
+        public void StopRead()
+        {
+
+            bw_serialRead.CancelAsync();
+           
+            
+        }
+        public string readerT(string current)
+        {
+            
+            return "";
+        }
+
         private void bw_serialRead_DoWork(object sender, DoWorkEventArgs e)
         {
-            try
-            {
-                port_1.Open();
-                while (port_1.IsOpen)
+            //try
+            //{
+
+            
+            if (port_1.IsOpen)
                 {
-                    int intBuffer;
-                    intBuffer = port_1.BytesToRead;
-                    byte[] byteBuffer = new byte[intBuffer];
-                    port_1.Read(byteBuffer, 0, intBuffer);
-                    CurrentValue = port_1.ReadLine();
-                    bw_serialRead.ReportProgress(0, true);
+                    port_1.Close();
+                }
+
+            //MessageBox.Show("Test");
+            try
+
+            { port_1.Open(); }
+            catch { }
+            
+            //MessageBox.Show("Testa");
+            while (port_1.IsOpen)
+                {
+                if (!port_1.IsOpen) { break; }
+                int intBuffer;
+                intBuffer = port_1.BytesToRead;
+                byte[] byteBuffer = new byte[intBuffer];
+                //var c = port_1.ReadExisting();
+                //var p=port_1.Read(byteBuffer, 0, intBuffer);
+                //MessageBox.Show(port_1.PortName);
+                //MessageBox.Show(current.Substring(1, current.Length-1));
+                //var bytes = Convert.FromBase64CharArray(current);
+                //string current="";
+                //try
+                //{
+                //    current = port_1.ReadLine();
+                //}
+                //catch (System.IO.IOException)
+                //{
+                //    Console.WriteLine(e.Argument);
+                //    Console.WriteLine(e.Result);
+                //    Console.WriteLine(e.Cancel) ;
+                //}
+
+                var current = "";
+                try
+                {
+                    current = port_1.ReadExisting();
+                }
+                catch (System.IO.IOException)
+                {
+                    Console.WriteLine(e.Argument);
+                    Console.WriteLine(e.Result);
+                    Console.WriteLine(e.Cancel);
+                }
+                string i = current;
+                if (i.Length == 16)
+                {
+                    if (i.Contains("-"))
+                    {
+                        string str = i.Substring(1, 7);
+                        CurrentValue = (Convert.ToInt32(str) * (-1)).ToString();
+                    }
+                    else
+                    {
+                        CurrentValue = i.Substring(0, 7).Trim();
+                    }
+                    if (i.Contains("MO"))
+                    {
+                        //MOtion detected
+
+                        motionDetected = true;
+                    }
+                    else
+                    {
+                        motionDetected = false;
+
+                    }
+                    if (i.Contains("CZ"))
+                    {
+                        //te value is 0 or on tare
+                        isTare = true;
+                    }
+                    else
+                    {
+                        isTare = false;
+                    }
+                    if ((i.Contains("LB")))
+                    {
+                        //Te units are not in lbs
+
+                        isLB = true;
+                    }
+                    else
+                    {
+                        isLB = false;
+                        
+                        if (i.Contains("-"))
+                        {
+                            string str = CurrentValue.Substring(1, 7);
+                            CurrentValue = (Convert.ToInt32(str) * (-1)).ToString();
+                        }
+                        //Good Value
+                    }
+                    //foreach (string j in current.Split('\n')) 
+                    //{
+                    //    //current.Substring(1,current.Length-1).Trim('\'').Replace("\\r","^").Split('^')
+                    //    string i = j;
+                    //        //MessageBox.Show(i);
+
 
 
                 }
-            }
-            catch (Exception)
-            {
-                port_1.Close();
-            }
-            
+                    //}
+                    Thread.Sleep(10);
+                bw_serialRead.ReportProgress(0, true);
+                }
+            //}
+            //catch (Exception ee)
+            //{
+            //    port_1.Close();
+            //    MessageBox.Show(ee.Message+ee.Source);
+            //    //bw_serialRead_DoWork(sender, e);
+            //}
+
         }
 
         private void bw_serialRead_ProgressChanged(object sender, ProgressChangedEventArgs e)
         {
             lbl_currentValue.Text = CurrentValue;
+            if (motionDetected)
+            {
+                lbl_motion.Text = lblm;
+            }
+            else
+            {
+                lbl_motion.Text = "";
+            }
         }
 
         private void ScaleSerialControl_Load(object sender, EventArgs e)
@@ -126,11 +281,20 @@ namespace ITXScaleSerialControl
         {
             try
             {
-                bw_serialRead.RunWorkerAsync();
-            }catch(Exception E)
+                //bw_serialRead.RunWorkerAsync();
+            }
+            catch(Exception E)
             {
-                Console.WriteLine(E.Message);
+                Console.WriteLine(E.Message+"Ccomplete ");
             }
         }
+
+        private void panelControl1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+
+
     }
 }
