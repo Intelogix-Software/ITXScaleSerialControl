4824ee8d8aed3b1c543e0809da9bdb5346d0fb70
RaulRSB <rauls@intelogix.mx>
2022-06-29 12:11:55 -0500

    +dev22

--- ITXScaleSerialControl/ScaleSerialControl.cs
+++ ITXScaleSerialControl/ScaleSerialControl.cs
@@ -1,14 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Drawing;
-using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing; 
 using System.Windows.Forms;
-using System.IO.Ports;
-using DevExpress.Utils;
+using System.IO.Ports; 
 using System.Threading;
 using System.IO;
 
