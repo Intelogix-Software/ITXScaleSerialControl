8b9d5e9a72cf47a4dafa1d3f61819ca9fc3abe88
Josue Reaza <101818049+JosueRea@users.noreply.github.com>
2023-02-27 11:11:28 -0600

    Correccion, agregado de rejex para traer el valor entero

--- ITXScaleSerialControl/ScaleSerialControl.cs
+++ ITXScaleSerialControl/ScaleSerialControl.cs
@@ -1,42 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Drawing; 
+using System.Drawing;
 using System.Windows.Forms;
-using System.IO.Ports; 
+using System.IO.Ports;
 using System.Threading;
 using System.IO;
+using System.Text.RegularExpressions;
 
 namespace ITXScaleSerialControl
 {
-    public partial class ScaleSerialControl: UserControl
+    public partial class ScaleSerialControl : UserControl
     {
-        public int fontSizeV=40;
-        //public new event EventHandler Click
-        //{
-        //    add
-        //    {
-        //        base.Click += value;
-        //        foreach (Control control in Controls)
-        //        {
-        //            control.Click += value;
-        //        }
-        //    }
-        //    remove
-        //    {
-        //        base.Click -= value;
-        //        foreach (Control control in Controls)
-        //        {
-        //            control.Click -= value;
-        //        }
-        //    }
-        //}
+        public int fontSizeV = 40;
 
         public bool hasImage { get; set; }
         private readonly MemoryStream _ms;
         public static string com { get; set; }
         public static Int16 baud { get; set; }
-        
+
         public void settings(bool HasImage, string printerCom, Int16 baudrate)
         {
 
@@ -46,19 +28,6 @@ namespace ITXScaleSerialControl
         }
         public void print(string alltext, byte[] image)
         {
-            //if (com == null || com=="") return ;
-            //var e = new EPSON();
-            //using (SerialPrinter printer = new SerialPrinter(portName: /*"COM7"*/ com, baudRate: baud))
-            //{
-            //    printer.DataAvailable();
-            //    printer.Write(
-            //      ByteSplicer.Combine(
-            //        //e.CenterAlign(),
-            //        hasImage ? e.PrintImage(image, true) : e.Print(""),
-            //        e.Print(alltext)
-            //        ));
-            //    Thread.Sleep(500);
-            //}
             SerialPort se = new SerialPort();
             se.PortName = com;
             se.BaudRate = 9600;
@@ -84,9 +53,9 @@ namespace ITXScaleSerialControl
                 foreach (Control control in this.Controls)
                 {
 
-                            control.DoubleClick += value;
+                    control.DoubleClick += value;
 
-                    foreach(Control control2 in control.Controls)
+                    foreach (Control control2 in control.Controls)
                     {
                         control2.DoubleClick += value;
                         foreach (Control control3 in control2.Controls)
@@ -94,7 +63,7 @@ namespace ITXScaleSerialControl
                             control3.DoubleClick += value;
                         }
                     }
-                } 
+                }
             }
             remove
             {
@@ -110,7 +79,7 @@ namespace ITXScaleSerialControl
                             control3.DoubleClick -= value;
                         }
                     }
-                } 
+                }
             }
         }
         private void Control_Click(object sender, EventArgs e)
@@ -127,7 +96,7 @@ namespace ITXScaleSerialControl
             get { return port_1.BaudRate; }
             set { port_1.BaudRate = value; }
         }
-        public bool lineread=false;
+        public bool lineread = false;
         public bool lread
         {
             get { return lineread; }
@@ -171,45 +140,29 @@ namespace ITXScaleSerialControl
         public bool motionDetected;
         public bool isTare;
         public bool isLB;
-        public bool cancel=false;
+        public bool cancel = false;
 
         private Queue<byte> recievedData = new Queue<byte>();
 
         public ScaleSerialControl()
         {
-            //Esto cambia todo el Thread, incluyendo la aplicacion en la que la llama
-            //Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en");
             InitializeComponent();
-            
-            //lbl_currentValue.Font = new Font(lbl_currentValue.Font.FontFamily,100);
-            //StartRead();
-
         }
         public void StartRead()
         {
-            
-            //port_1 = new SerialPort();
-            //port_1.BaudRate =SerialPortBaud;
-            //port_1.PortName = SerialPortCOMName;
             bw_serialRead.RunWorkerAsync();
-            //port_1.Open(); 
-            //System.Windows.Forms.MessageBox.Show(port_1.PortName);
-            
+
         }
         public void StopRead()
         {
-            
-
-            //bw_serialRead.CancelAsync();
             if (port_1.IsOpen)
             {
                 port_1.Close();
             }
-            //MessageBox.Show(port_1.IsOpen.ToString());
         }
         public string readerT(string current)
         {
-            
+
             return "";
         }
         public void restartPort()
@@ -219,60 +172,39 @@ namespace ITXScaleSerialControl
                 port_1.Close();
             }
             Thread.Sleep(500);
-            try { port_1.Open(); } catch (Exception ex) { Console.WriteLine(ex.Message); } 
+            try { port_1.Open(); } catch (Exception ex) { Console.WriteLine(ex.Message); }
         }
         private void bw_serialRead_DoWork(object sender, DoWorkEventArgs e)
         {
-            //try
-            //{ 
             if (port_1.IsOpen)
-                {
-                    port_1.Close();
-                }
+            {
+                port_1.Close();
+            }
 
-            //MessageBox.Show("Test");
             try
 
             { port_1.Open(); }
             catch { }
-            
-            //MessageBox.Show("Testa");
+
             while (port_1.IsOpen)
-                {
+            {
                 if (!port_1.IsOpen) { break; }
                 int intBuffer;
                 intBuffer = port_1.BytesToRead;
                 byte[] byteBuffer = new byte[intBuffer];
-                //var c = port_1.ReadExisting();
-                //var p=port_1.Read(byteBuffer, 0, intBuffer);
-                //MessageBox.Show(port_1.PortName);
-                //MessageBox.Show(current.Substring(1, current.Length-1));
-                //var bytes = Convert.FromBase64CharArray(current);
-                //string current="";
-                //try
-                //{
-                //    current = port_1.ReadLine();
-                //}
-                //catch (System.IO.IOException)
-                //{
-                //    Console.WriteLine(e.Argument);
-                //    Console.WriteLine(e.Result);
-                //    Console.WriteLine(e.Cancel) ;
-                //}
 
                 var current = "";
-
 
                 if (lineread)
                 {
                     string s = "";
                     try
                     {
-                        current = port_1.ReadLine() ;
+                        current = port_1.ReadLine();
 
                         if (current.Length == 0) return;
-                        s = current.Substring(1, current.Length -1).Split('L')[0];
-                        
+                        s = current.Substring(1, current.Length - 1).Split('L')[0];
+
                     }
                     catch (System.IO.IOException)
                     {
@@ -283,8 +215,9 @@ namespace ITXScaleSerialControl
                     }
 
                     string i = s.Trim();
-                    
-                    if (current.Contains("M"))
+
+
+                    if (current.Contains("M") || current.Contains("MO"))
                     {
                         //MOtion detected
 
@@ -304,7 +237,7 @@ namespace ITXScaleSerialControl
                     {
                         isTare = false;
                     }
-                    if ((current.Contains("LB"))|| current.Contains("LG"))
+                    if ((current.Contains("LB")) || current.Contains("LG"))
                     {
                         //Te units are not in lbs
 
@@ -315,15 +248,10 @@ namespace ITXScaleSerialControl
                         isLB = false;
                         //Good Value
                     }
-                    CurrentValue = i.Replace(" ","");
-
-
-
+                    CurrentValue = Regex.Match(i.Replace(" ", ""), @"\d+").Value;
                 }
                 else
                 {
-
-
                     try
                     {
                         current = port_1.ReadExisting();
@@ -337,16 +265,17 @@ namespace ITXScaleSerialControl
                     }
 
                     string i = current;
-                    if (i.Length == 16 )
+                    if (i.Length == 16)
                     {
                         if (i.Contains("-"))
                         {
-                            string str = i.Substring(1, 7);
-                            CurrentValue = (Convert.ToInt32(str) * (-1)).ToString();
+                            //string str = i.Substring(1, 7);
+                            CurrentValue = Regex.Match(i.Replace(" ", ""), @"\d+").Value;
                         }
                         else
                         {
-                            CurrentValue = i.Substring(0, 7).Trim();
+                            //CurrentValue = i.Substring(0, 7).Trim();
+                            CurrentValue = Regex.Match(i.Replace(" ", ""), @"\d+").Value;
                         }
                         if (i.Contains("M"))
                         {
@@ -368,7 +297,7 @@ namespace ITXScaleSerialControl
                         {
                             isTare = false;
                         }
-                        if (i.Contains("LB")|| i.Contains("L"))
+                        if (i.Contains("LB") || i.Contains("L"))
                         {
                             //Te units are not in lbs
 
@@ -380,39 +309,22 @@ namespace ITXScaleSerialControl
 
                             if (i.Contains("-"))
                             {
-                                string str = CurrentValue.Substring(1, 7);
-                                CurrentValue = (Convert.ToInt32(str) * (-1)).ToString();
+                                //string str = CurrentValue.Substring(1, 7);
+                                CurrentValue = "-" + Regex.Match(i.Replace(" ", ""), @"\d+").Value;
                             }
                             //Good Value
                         }
-                        //foreach (string j in current.Split('\n')) 
-                        //{
-                        //    //current.Substring(1,current.Length-1).Trim('\'').Replace("\\r","^").Split('^')
-                        //    string i = j;
-                        //        //MessageBox.Show(i);
-
-
-
                     }
                 }
-                    //}
-                    Thread.Sleep(10);
+                //}
+                Thread.Sleep(10);
                 bw_serialRead.ReportProgress(0, true);
-                
-                }
-            //}
-            //catch (Exception ee)
-            //{
-            //    port_1.Close();
-            //    MessageBox.Show(ee.Message+ee.Source);
-            //    //bw_serialRead_DoWork(sender, e);
-            //}
 
-
+            }
         }
-        delegate void SetTextCallback(string text,bool v);
+        delegate void SetTextCallback(string text, bool v);
 
-        private void SetText(string text,bool v)
+        private void SetText(string text, bool v)
         {
             // InvokeRequired required compares the thread ID of the
             // calling thread to the thread ID of the creating thread.
@@ -463,7 +375,7 @@ namespace ITXScaleSerialControl
 
         private void bw_serialRead_ProgressChanged(object sender, ProgressChangedEventArgs e)
         {
-            SetText(CurrentValue,motionDetected);
+            SetText(CurrentValue, motionDetected);
             if (motionDetected)
             {
                 SetTextM(lblm);
